257b89960fd30507ad603e518b61687f9c6ac42e
src-thk <sarthak-tandon@outlook.com>
2020-12-13 09:51:10 +0530

    user-registeration frontend completed

--- src/Authentication.vue
+++ src/Authentication.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <Signup />
-    <Login />
+    <div v-if="isToggle">
+      <div class="auth-header">
+        <h1>Signup</h1>
+        <div class="btt-header">
+          <button @click="isToggle = !isToggle">Login instead?</button>
+        </div>
+      </div>
+      <Signup />
+    </div>
+    <div v-else>
+      <div class="auth-header">
+        <h1>Login</h1>
+        <div class="btt-header">
+          <button @click="isToggle = !isToggle">Register instead?</button>
+        </div>
+      </div>
+      <Login />
+    </div>
   </div>
 </template>
 
@@ -9,8 +25,24 @@
 import Signup from './components/Signup.vue';
 import Login from './components/Login.vue';
 export default {
+  data() {
+    return {
+      isToggle: false,
+    };
+  },
   components: { Login, Signup },
 };
 </script>
 
-<style></style>
+<style>
+.auth-header {
+  display: block;
+  text-align: center;
+  margin: 50px;
+}
+.btt-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
